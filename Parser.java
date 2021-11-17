030087d7578398841964ed3a7756908e313bc996
GehadAkram <63369821+GehadAkram@users.noreply.github.com>
2021-11-17 20:29:28 +0200

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -2,12 +2,12 @@ public class Parser {
     String commandName;
     String[] args;
 
-//This method will divide the input into commandName and args
-//where "input" is the string command entered by the user 
+    //This method will divide the input into commandName and args
+    //where "input" is the string command entered by the user 
     public boolean parse(String input) {
         String parts[] = input.split("[ ]");
         commandName = parts[0];
-        String[] args = new String[(parts.length)-1];
+        args = new String[(parts.length)-1];
         for (int i = 0; i < parts.length-1; i++) {
             args[i] = parts[i+1];
         }
